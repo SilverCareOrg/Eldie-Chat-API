cc297c946f52f9d79a5f4f45f347a93a5eac0622
avramstefan <94166855+avramstefan@users.noreply.github.com>
2024-07-06 23:07:32 +0300

    Added all hosts

--- eldie_chat_api/eldie_chat_api/settings.py
+++ eldie_chat_api/eldie_chat_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-((1^d^_4)brm*9ou0c!&*&%5=i_kg2b39n-_ye12@(m2f4%+7m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 openai_client = OpenAI(api_key=env('OPENAI_API_KEY'))
 
